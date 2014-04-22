cbb04d86158f4043e1a436ee0cbbfa00f383f975
unknown <zhangjinlai@DST56181.cn1.global.ctrip.com>
2014-04-22 14:53:00 +0800

    Project initial

---
+++ GS.Solution.TfsExtensions.Core/TfsConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GS.Solution.TfsExtensions.Core
+{
+    public class TfsConnection
+    {
+
+    }
+}
