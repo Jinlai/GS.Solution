c27852bf156af64c8e689cf0ebf35167a98f1be2
unknown <zhangjinlai@DST56181.cn1.global.ctrip.com>
2014-04-23 14:18:43 +0800

    Reach to OutputWindowPane of vs

---
+++ GS.Solution.VsExtensions/OutputWindowPaneExtensions.cs
@@ -0,0 +1,21 @@
+﻿/*
+ *  created by zhangjinlai@ctrip.com
+ *  date 4/23/2014 2:04:02 PM
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EnvDTE;
+
+namespace GS.Solution.VsExtensions
+{
+    public static class OutputWindowPaneExtensions
+    {
+        public static void OutputLine(this OutputWindowPane outputWindowPane, string text)
+        {
+            outputWindowPane.OutputString(String.Format("{0}\r\n", text));
+        }
+    }
+}
---
+++ GS.Solution.VsExtensions/OutputWindowPaneProvider.cs
@@ -0,0 +1,30 @@
+﻿/*
+ *  created by zhangjinlai@ctrip.com
+ *  date 4/23/2014 10:53:23 AM
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GS.Solution.VsExtensions
+{
+    using EnvDTE;
+    using EnvDTE80;
+
+    public static class OutputWindowPaneProvider
+    {
+        private const string OutputWindowPaneName = "GS.Solution.Templates.OutputWindowPane";
+
+        public static OutputWindowPane GetOutputWindowPane(DTE2 dte2)
+        {
+            var outputWindow = dte2.Windows.Item(Constants.vsWindowKindOutput).Object as OutputWindow;
+
+            //return outputWindow.OutputWindowPanes.Item(OutputWindowPaneName) ?? outputWindow.OutputWindowPanes.Add(OutputWindowPaneName);
+
+            return outputWindow.OutputWindowPanes.Add(OutputWindowPaneName);
+        }
+    }
+}
