810f874e06192914c4c555d351b23614444627de
Jared <jinlai76@gmail.com>
2014-05-15 18:21:06 +0800

    ProjectType.cs file added

---
+++ GS.Solution.VsExtensions/Project/ProjectType.cs
@@ -0,0 +1,19 @@
+﻿/*
+ *  created by zhangjinlai@ctrip.com
+ *  date 5/15/2014 5:33:21 PM
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GS.Solution.VsExtensions.Project
+{
+    public class ProjectType
+    {
+        public const string ClassLibraryProjectTemplate = "ClassLibrary.zip";
+
+        public const string EmptyMvcWebApplicationProjectTemplatev3 = "EmptyMvcWebApplicationProjectTemplatev3.01.cshtml.zip";
+    }
+}
