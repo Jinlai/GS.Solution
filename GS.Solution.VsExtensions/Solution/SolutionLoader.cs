02fc49c5ee3b721075b040e5b3d1fff2de3ec501
Jared <jinlai76@gmail.com>
2014-05-22 13:39:57 +0800

    add a file that missed

---
+++ GS.Solution.VsExtensions/Solution/SolutionLoader.cs
@@ -0,0 +1,21 @@
+﻿/*
+ *  created by zhangjinlai@ctrip.com
+ *  date 4/29/2014 10:26:40 AM
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GS.Solution.VsExtensions.Solution
+{
+    public class SolutionLoader
+    {
+        public static void Load()
+        {
+
+        }
+    }
+}
